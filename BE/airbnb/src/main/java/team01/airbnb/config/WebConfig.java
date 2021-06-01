35cbf1d7281feee5c6a56c294984ab5c5f2b8500
Shion <ehdrhelr@gmail.com>
2021-06-01 14:39:17 +0900

    [#5] feat : 프론트 로컬서버의 리소스 요청을 허용하라.

--- BE/airbnb/src/main/java/team01/airbnb/config/WebConfig.java
+++ BE/airbnb/src/main/java/team01/airbnb/config/WebConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -18,6 +19,11 @@ import java.util.List;
 public class WebConfig implements WebMvcConfigurer {
 
     private final LoginUserArgumentResolver loginUserArgumentResolver;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
